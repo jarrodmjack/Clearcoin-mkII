e30497da02c45403284f1f2908ffe2488e3dee7e
jarrodmjack <jarrodmjack@gmail.com>
2022-10-06 12:52:20 -0400

    added initial authentication check to Auth context and conditional rendering to Navbar

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer, useEffect } from 'react'
 
 export const AuthContext = createContext()
 
@@ -17,6 +17,15 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
+
+    // check for token in local storage to see if there is already a user logged in
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'))
+
+        if(user) {
+            dispatch({ type: 'LOGIN', payload: user })
+        }
+    }, [])
 
     console.log('AuthContext state: ', state)
     return (
