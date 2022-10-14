a8d81ec96519ba4e403d99da6fa67aeb6e487f95
jarrodmjack <jarrodmjack@gmail.com>
2022-10-14 00:17:55 -0400

    changed mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,6 @@ const transactionRoutes = require('./routes/transactions')
 const userRoutes = require('./routes/userRoutes')
 const path = require('path')
 
-//Use .env file in config folder
-require("dotenv").config();
-
-// express app
-const app = express()
-
-// middleware
-app.use(express.json())
-
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
-// routes
-
-app.use('/api/transactions', transactionRoutes)
-app.use('/api/user', userRoutes)
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-app.get("*", (req, res) =>
-  res.sendFile(
-    path.resolve(__dirname, "../", 'frontend', 'build', 'index.html')
-  )
-)
-
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
@@ -42,3 +17,31 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((error) => {
     console.log(error)
   })
+  .finally(() => console.log('mongo_URI><><><><><', process.env.MONGO_URI))
+
+
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  // console.log(req.path, req.method)
+  next()
+})
+
+// routes
+
+app.use('/api/transactions', transactionRoutes)
+app.use('/api/user', userRoutes)
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "../", 'frontend', 'build', 'index.html')
+  )
+)
+
+
