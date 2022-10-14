fb848f69343d9277baecb6fea1cd8a2e4980f39b
jarrodmjack <jarrodmjack@gmail.com>
2022-10-13 21:24:13 -0400

    added dirname into server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use('/api/transactions', transactionRoutes)
 app.use('/api/user', userRoutes)
-app.use(express.static('../frontend/build'))
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 app.get("*", (req, res) =>
   res.sendFile(
     path.resolve(__dirname, "../", 'frontend', 'build', 'index.html')
